4ce61fc3be18cc468d5dff808fe3be8782981db8
JeFi <jack31415926535@gmail.com>
2024-11-06 06:48:03 +0800

    Add conditional check to OldManEntity Draw patch and add fallback transpiler method
    This update introduces a condition to the Draw patch in OldManEntity to reduce unnecessary flushes. Also includes a fallback transpiler method for additional flexibility.

--- MirrorMode/Patching/OldManEntity.cs
+++ MirrorMode/Patching/OldManEntity.cs
@@ -1,13 +1,22 @@
 using HarmonyLib;
 using System;
 using System.Reflection;
-using Microsoft.Xna.Framework;
+// using System.Reflection.Emit;
+// using System.Collections.Generic;
+// using System.Diagnostics;
+// using System.Linq;
+// using JK = JumpKing;
+// using Microsoft.Xna.Framework;
+
 using MirrorMode.Models;
+using JumpKing;
 
 namespace MirrorMode.Patching
 {
     public class OldManEntity
     {
+        private static FieldInfo m_settings;
+        private static FieldInfo home_screen;
         public OldManEntity (Harmony harmony)
         {
             Type type = Type.GetType("JumpKing.MiscEntities.OldManEntity, JumpKing");
@@ -15,6 +24,7 @@ namespace MirrorMode.Patching
             harmony.Patch(
                 Draw,
                 prefix: new HarmonyMethod(AccessTools.Method(typeof(OldManEntity), nameof(preDraw))),
+                // transpiler: new HarmonyMethod(AccessTools.Method(typeof(OldManEntity), nameof(transpilerDraw)))
                 postfix: new HarmonyMethod(AccessTools.Method(typeof(OldManEntity), nameof(postDraw)))
             );
             
@@ -26,13 +36,61 @@ namespace MirrorMode.Patching
             harmony.Patch(
                 DrawText,
                 prefix: new HarmonyMethod(AccessTools.Method(typeof(OldManEntity), nameof(preDrawText)))
-                // postfix: new HarmonyMethod(AccessTools.Method(typeof(OldManEntity), nameof(postDrawText)))
             );
+
+            m_settings = type.GetField("m_settings", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public);
+            home_screen = typeof(JumpKing.MiscEntities.OldMan.OldManSettings).GetField("home_screen");
         }
 
-        private static void preDraw() 
+        // private static IEnumerable<CodeInstruction> transpilerDraw(IEnumerable<CodeInstruction> instructions) {
+        //     CodeMatcher matcher = new CodeMatcher(instructions /*, ILGenerator generator*/);
+
+        //     try {
+        //         matcher.MatchStartForward(
+        //                 new CodeMatch(OpCodes.Beq_S),
+        //                 new CodeMatch(OpCodes.Ret)
+        //             )
+        //             .ThrowIfInvalid("123");
+        //         Label label = (Label)matcher.Operand;
+        //         matcher.Advance(2)
+        //             .Insert(
+        //                 CodeInstruction.Call(() => preDraw())
+        //             );
+        //         matcher.Instruction.labels.Add(label);
+        //         matcher.Advance(1).Instruction.labels.Clear();
+                
+        //         matcher.MatchStartForward(new CodeMatch(OpCodes.Ret))
+        //             .ThrowIfInvalid("456");
+        //         List<Label> labels = new List<Label>(matcher.Instruction.labels);
+        //         if (labels.Count != 0) {
+        //             matcher.Instruction.labels.Clear();
+        //             matcher.Insert(
+        //                 CodeInstruction.Call(() => postDraw())
+        //             );
+        //             matcher.Instruction.labels = labels;
+        //         }
+        //         else {
+        //             matcher.Insert(
+        //                 CodeInstruction.Call(() => postDraw())
+        //             );
+        //         }
+        //     } catch (Exception e) {
+        //         Debug.WriteLine($"[ERROR] {e.Message}");
+        //         return instructions;
+        //     }
+
+        //     foreach (CodeInstruction i in matcher.Instructions()) {
+        //         Debug.WriteLine(i.ToString());
+        //     }
+        //     return matcher.Instructions();
+        // }
+
+        private static void preDraw(object __instance) 
         {
-            if (SpriteBatchManager.isMirror && !SpriteBatchManager.isMirroring)
+            // Debug.WriteLine((int)home_screen.GetValue(m_settings.GetValue(__instance)));
+            // Debug.WriteLine(Camera.CurrentScreen);
+            // Debugger.Break();
+            if ((int)home_screen.GetValue(m_settings.GetValue(__instance)) == Camera.CurrentScreenIndex1 && SpriteBatchManager.isMirror && !SpriteBatchManager.isMirroring)
             {
                 SpriteBatchManager.Switch2MirrorBatch();
             }
@@ -46,17 +104,9 @@ namespace MirrorMode.Patching
             }
         }
 
-        // private static void postDrawText() 
-        // {
-        //     if (SpriteBatchManager.isMirror && !SpriteBatchManager.isMirroring)
-        //     {
-        //         SpriteBatchManager.Switch2MirrorBatch();
-        //     }
-        // }
-
-        private static void postDraw() 
+        private static void postDraw(object __instance) 
         {
-            if (SpriteBatchManager.isMirror && !SpriteBatchManager.isMirroring)
+            if ((int)home_screen.GetValue(m_settings.GetValue(__instance)) == Camera.CurrentScreenIndex1 && SpriteBatchManager.isMirror && !SpriteBatchManager.isMirroring)
             {
                 SpriteBatchManager.Switch2NormalBatch();
                 SpriteBatchManager.FlushMirror(mirrorFirst: true);
